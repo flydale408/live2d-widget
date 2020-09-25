d746a4483947967ba36376af659cfea57d8d3d7c
flydale408 <383053277@qq.com>
2020-09-25 15:46:32 +0800

    更新

--- autoload.js
+++ autoload.js
@@ -1,12 +1,12 @@
 // 注意：live2d_path 参数应使用绝对路径
-// const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "/live2d-widget/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/flydale408/live2d-widget@latest/";
+// const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
 		let tag;
-
+		
 		if (type === "css") {
 			tag = document.createElement("link");
 			tag.rel = "stylesheet";
